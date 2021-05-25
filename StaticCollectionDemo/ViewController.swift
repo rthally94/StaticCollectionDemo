77c1c749928870b2a322ccae60541aa5e4d1ca95
Ryan Thally <rthally94@gmail.com>
2021-05-25 09:58:02 -0400

    Initial Commit

---
+++ StaticCollectionDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StaticCollectionDemo
+//
+//  Created by Ryan Thally on 5/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
